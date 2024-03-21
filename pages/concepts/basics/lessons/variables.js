98fd04384c6bc214c53359414d5510229cb57edf
Nick A Lanese <58953235+NLanese@users.noreply.github.com>
2024-03-20 23:55:49 -0400

    Update variables.js

--- pages/concepts/basics/lessons/variables.js
+++ pages/concepts/basics/lessons/variables.js
@@ -3,9 +3,36 @@ import React, {useEffect, useState} from "react";
 
 // Styles
 import Styles from "../../../../styles/styles";
+ 
+// Recoil
+import { directoryDataState } from "../../../../recoil/atoms";
 
 
 const VariablesLesson = ({}) => {
+
+    ///////////
+    // State //
+    ///////////
+
+    const [directory, setDirectory] = useRecoilState(directoryDataState)
+
+    const [loading, setLoading] = useState(true)
+
+    ////////////////
+    // UseEffects //
+    ////////////////
+
+    // Sets Directory if not already
+    useEffect(() => {
+        if (directory !== "Basics"){
+            setDirectory("Basics")
+        }
+        setLoading(false)
+    }, [])
+ 
+    ///////////////
+    // Rendering //
+    ///////////////
 
 }
 
