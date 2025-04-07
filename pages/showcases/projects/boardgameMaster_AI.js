60136c6a1443f4729062479e912577ed129190cc
Nick A Lanese <58953235+NLanese@users.noreply.github.com>
2025-04-07 02:47:06 -0400

    Create boardgameMaster_AI.js

---
+++ pages/showcases/projects/boardgameMaster_AI.js
@@ -0,0 +1,48 @@
+// React
+import React, {useEffect, useState} from "react";
+
+// Recoil
+import { useRecoilState } from "recoil";
+import { tokenState, tabBarState, directoryDataState } from "../../../recoil/atoms";
+
+// Styles 
+import Styles from "../../../styles/styles";
+
+// Ostrich
+import { OstCard } from "../../../OstrichComponents/Format/OstCard";
+import { OstrichDropDown } from "../../../OstrichComponents/Dropdown/OstrichDropDown";
+import { OstrichTabBar } from "../../../OstrichComponents/Tabs/OstrichTabBar";
+import { HeaderBar } from "../../../components/Global/headerBar";
+
+export default function BoardGameMasterAIProjectPage() {
+
+
+return (
+    <div style={{marginTop: 20}}>
+     <div style={Styles.Fonts.pageTitle}>The Ostrich React Component Library</div>
+     <div style={{display: 'flex', flexDirection: 'row'}}>
+        <OstCard style={{margin: "30px", flex: 10}}>
+            <div style={Styles.Sections.lessonContent}>
+                <p style={Styles.Fonts.basic}>
+                This entire Site has been made using the Ostrich React Component Library and regular Vanilla React tags. 
+                This means no other frameworks were used and this means developers who import our Ostrich Component Library can easily create a well designed site 
+                of their own!
+                </p>
+
+                <p style={Styles.Fonts.basic}>
+                Ostrich Components were made with the intent of heavily reducing the amount of time new developers have to spend on certain Front End aspects of development. 
+                Using a multitude of optional props for each component, developers can have everything from extremely high functionality components with minimal development to extremely simple and low level components that come already styled. 
+                </p>
+            </div>
+            
+        </OstCard>
+
+
+        {/* Side Menu */}
+        <OstCard style={{marginTop: 30, marginRight: 30, flex: 2}}>
+
+        </OstCard>
+     </div>
+    </div>
+  );
+}
