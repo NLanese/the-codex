38f53836b66c6c27dd91598279e761428251bd34
Nick A Lanese <58953235+NLanese@users.noreply.github.com>
2025-04-08 00:13:33 -0400

    Update boardgameMaster_AI.js

--- pages/showcases/projects/boardgameMaster_AI.js
+++ pages/showcases/projects/boardgameMaster_AI.js
@@ -48,41 +48,68 @@ export default function BoardGameMasterAIProjectPage() {
 
 return (
     <div style={{marginTop: 20}}>
-     <div style={Styles.Fonts.pageTitle}>The Board Game Master AI</div>
-     <div style={{display: 'flex', flexDirection: 'row'}}>
-        <OstCard style={{margin: "30px", flex: 12}}>
-            <div style={Styles.Sections.lessonContent}>
-                <p style={Styles.Fonts.basic}>
-                Below is a Chat Bot designed to answer your questions about any of the board games we have listed! 
-                Simply select a board game from the dropdown menu and enter your question in the text field. 
-                Once complete, press submit and wait for the Board Game Master to give you a response!
-                </p>
+        <div style={Styles.Fonts.pageTitle}>The Board Game Master AI</div>
 
-                <p style={Styles.Fonts.basic}>
-                This bot was deisgned using Amazon Bedrock. Its knowledge base was compiled by Nick Lanese but all the content was 
-                information from the respecitve board games' official rules (when applicable).
-                </p>
+            <div style={{display: 'flex', flexDirection: 'row'}}>
+                <OstCard style={{margin: "30px", flex: 12}}>
+                    <div style={Styles.Sections.lessonContent}>
+                        <p style={Styles.Fonts.basic}>
+                        Below is a Chat Bot designed to answer your questions about any of the board games we have listed! 
+                        Simply select a board game from the dropdown menu and enter your question in the text field. 
+                        Once complete, press submit and wait for the Board Game Master to give you a response!
+                        </p>
 
-                <p style={Styles.Fonts.basic}>
-                The LLM at use is Titan Text Express and I am using Titan Embedding v2 to extract the information from the knowledge base. 
-                </p>
+                        <p style={Styles.Fonts.basic}>
+                        This bot was deisgned using Amazon Bedrock. Its knowledge base was compiled by Nick Lanese but all the content was 
+                        information from the respecitve board games' official rules (when applicable).
+                        </p>
+
+                        <p style={Styles.Fonts.basic}>
+                        The LLM at use is Titan Text Express and I am using Titan Embedding v2 to extract the information from the knowledge base. 
+                        </p>
+                    </div>
+                </OstCard>
             </div>
 
-            <div style={{...Styles.Sections.lessonContent, justifyContent: 'center', display: 'flex'}}>
-                <OstrichDropDown 
-                    boxStyle={{width: 400}}
-                    title={determineDropTitle()}
-                    drawers={[
-                        "Checkers",
-                        "Chess",
-                        "Splendor",
-                        "Robo Quest Arena"
-                    ]}
-                />
+            <div style={{...Styles.Sections.lessonContent, alignItems: 'center', display: 'flex', flexDirection: 'column'}}>
+                <div>
+                    <OstrichDropDown 
+                        boxStyle={{width: 400, alignContent: 'center'}}
+                        title={determineDropTitle()}
+                        titleStyle={{fontSize: 20}}
+                        onDrawerClick={(drawer) => setSelGame(drawer)}
+                        drawers={[
+                            "Checkers",
+                            "Chess",
+                            "Splendor",
+                            "Robo Quest Arena"
+                        ]}
+                    />
+                </div>
+                
+                <OstCard style={{marginTop: 50, height: '45vw', width: '85%'}}>
+                    <OstCard style={{marginTop: '3%', height: '70%', width: '90%', marginLeft: '5%', borderRadius: 0, backgroundColor: '#ededed'}}>
+
+                    </OstCard>
+                    <textarea 
+                        type="textarea" 
+                        onChange={(event) => console.log(event.target)} 
+                        style={{
+                            marginTop: '3%', 
+                            height: '14%', 
+                            width: '95%', 
+                            marginLeft: '2.5%', 
+                            borderRadius: 0, 
+                            backgroundColor: '#eee',
+                            fontSize: 18,
+                            padding: 20,
+                            boxSizing: 'border-box',
+                            verticalAlign: 'top',
+                            fontFamily: 'Gilroy'
+                        }}
+                    />
+                </OstCard>
             </div>
-            
-        </OstCard>
-     </div>
     </div>
   );
 }
