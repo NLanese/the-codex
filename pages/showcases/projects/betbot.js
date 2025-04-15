8dc3eb1e52ed295133247a4c237cd4d684322e5e
Nick A Lanese <58953235+NLanese@users.noreply.github.com>
2025-04-14 23:28:07 -0400

    Update betbot.js

--- pages/showcases/projects/betbot.js
+++ pages/showcases/projects/betbot.js
@@ -80,24 +80,22 @@ return (
         <div style={Styles.Fonts.pageTitle}>The NBA BetBot </div>
 
         <div style={{display: 'flex', flexDirection: 'row'}}>
-            <OstCard style={{margin: "30px", flex: 12}}>
-                <div style={Styles.Sections.lessonContent}>
-                    <p style={Styles.Fonts.basic}>
-                    Below is a Chat Bot designed to answer your questions about any of the board games we have listed! 
-                    Simply select a board game from the dropdown menu and enter your question in the text field. 
-                    Once complete, press submit and wait for the Board Game Master to give you a response!
-                    </p>
+            <div style={{...Styles.Sections.lessonContent, width: '70%', marginLeft: '15%', marginTop: 45}}>
+                <p style={Styles.Fonts.basic}>
+                Below is a Chat Bot designed to answer your questions about any of the board games we have listed! 
+                Simply select a board game from the dropdown menu and enter your question in the text field. 
+                Once complete, press submit and wait for the Board Game Master to give you a response!
+                </p>
 
-                    <p style={Styles.Fonts.basic}>
-                    This bot was deisgned using Amazon Bedrock. Its knowledge base was compiled by Nick Lanese but all the content was 
-                    information from the respecitve board games' official rules (when applicable).
-                    </p>
+                <p style={Styles.Fonts.basic}>
+                This bot was deisgned using Amazon Bedrock. Its knowledge base was compiled by Nick Lanese but all the content was 
+                information from the respecitve board games' official rules (when applicable).
+                </p>
 
-                    <p style={Styles.Fonts.basic}>
-                    The LLM at use is Titan Text Express and I am using Titan Embedding v2 to extract the information from the knowledge base. 
-                    </p>
-                </div>
-            </OstCard>
+                <p style={Styles.Fonts.basic}>
+                The LLM at use is Titan Text Express and I am using Titan Embedding v2 to extract the information from the knowledge base. 
+                </p>
+            </div>
         </div>
     </div>
   );
