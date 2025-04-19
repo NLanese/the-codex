c799acd04f33ac6b51e9a849fbd5426633e41ac8
Nick A Lanese <58953235+NLanese@users.noreply.github.com>
2025-04-19 02:16:05 -0400

    Update betbot.js

--- pages/showcases/projects/betbot.js
+++ pages/showcases/projects/betbot.js
@@ -28,6 +28,9 @@ export default function BetBotProjectPage() {
         // All Scraped Bets
         const [bets, setBets] = useState(false)
 
+        // Current Staged Bet Sheet
+        const [stagedBetSheet, setStagedBetSheet] = useState(false)
+
         // Trigger for Add Bet Modal
         const [addBetModal, setAddBetModal] = useState(false)
 
@@ -293,122 +296,6 @@ export default function BetBotProjectPage() {
             )
         }
 
-        // Renders all FanDuel Bets as individual Cards
-        function renderBetCards(){
-            if (!bets || bets.length < 1){
-                return
-            }
-            let fullRenderList = []
-            let row = []
-            let i = 0
-            bets.forEach((betCard, index) => {
-                const awayLine = betCard?.bet?.moneyline?.[betCard.away] ? betCard?.bet?.moneyline?.[betCard.away] : false
-                const homeLine = betCard?.bet?.moneyline?.[betCard.home] ? betCard?.bet?.moneyline?.[betCard.home] : false
-
-                const awayDiff = betCard?.bet?.spread?.[betCard.away]?.diff ? betCard?.bet?.spread?.[betCard.away]?.diff : false
-                const homeDiff = betCard?.bet?.spread?.[betCard.home]?.diff ? betCard?.bet?.spread?.[betCard.home]?.diff : false
-
-                const awaySpreadOdds = betCard?.bet?.spread?.[betCard.away]?.odds ? betCard?.bet?.spread?.[betCard.away]?.odds : "No Spread Betting"
-                const homeSpreadOdds = betCard?.bet?.spread?.[betCard.home]?.odds ? betCard?.bet?.spread?.[betCard.home]?.odds : "No Spread Betting"
-
-                const tipoff = betCard.tipoff
-
-                // if (!awayLine && !awayDiff){
-                //     return false
-                // }
-
-                row = [
-                    ...row, 
-                    (
-                    <OstCard 
-                    style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', flex: 4}}
-                    >
-                        <div style={{borderRadius: 15, paddingBottom: 0, marginBottom: 15, paddingRight: '3.3%', paddingLeft: '3.3%', backgroundColor: "#11013b"}}>
-                            <p style={{...Styles.Fonts.basic, color: '#efefef', lineHeight: 1, margin: 2, padding: 2, fontSize: 24, overflow: "hidden"}}>
-                                {betCard.away} @ {betCard.home}
-                            </p>
-                            <p style={{...Styles.Fonts.basic, color: '#efefef', lineHeight: 1, margin: 2, padding: 2, paddingTop: 5}}>
-                                {renderDate(betCard.tipoff)}
-                            </p>
-                        </div>
-                        <div style={{display: 'flex', flexDirection: 'row', gap: 10}}>
-                            {renderTeamBetCard(betCard.away, awayLine, awayDiff, awaySpreadOdds, tipoff)}
-                            {renderTeamBetCard(betCard.home, homeLine, homeDiff, homeSpreadOdds)}
-                        </div>
-                    </OstCard>
-                    )
-                ]
-                i = i + 1
-                if (i == 3 || index >= bets.length - 1){
-                    fullRenderList = [
-                        ...fullRenderList,
-                        (
-                            <div style={{
-                                display: 'flex', flexDirection: 'row', gap: 33,
-                                width: '95%', marginLeft: '2.5%', 
-                                borderTop: "4px solid #11013b", marginBottom: 20, paddingTop: 20
-                            }}>
-                                {row}
-                            </div>
-                        )
-                    ]
-                    row = []
-                    i = 0
-                }
-            })
-            return fullRenderList  
-        }
-
-        // Takes UTC Date String and Returns it Legibly
-        function renderDate(dateTime){
-            let dateArray = dateTime.split("-")
-            let time = dateArray[2].split("T")[1]
-            dateArray[2] = dateArray[2].split("T")[0]
-            dateArray = [...dateArray, time]
-            return `${dateArray[1]}-${dateArray[2]} at ${renderTime(dateArray[3])}`
-        }
-
-        // Takes Military Time and Converts it 
-        function renderTime(time){
-            let ampm = "am"
-            let hour = parseInt(time.split(":")[0], 10)
-            if (hour > 12){
-                hour = hour - 12
-                ampm = "pm"
-            }
-            if (hour == 0){
-                hour = 12
-            }
-            return `${hour}:${time.split(":")[1]}${ampm}`
-        }
-
-        // Renders a single Moneyline / Spread Sheet
-        function renderTeamBetCard(team, line, points, spreadLine){
-            return(
-                <OstCard 
-                style={{flex: 5, height: 230}}
-                onClick={(team, line, points, spread) => {
-
-                }}
-                >
-                    <p style={{...Styles.Fonts.h2, fontSize: 28, paddingBottom: 10, height: 40}}>
-                        {team}
-                    </p>
-                    <p style={{...Styles.Fonts.basic, fontSize: 20, textAlign: 'center'}}>
-                        Moneyline: <span style={{color: 'darkgrey', fontWeight: 500}}>{line ? line : "No Moneyline"}</span>
-                    </p>
-                    <div>
-                        <p style={{...Styles.Fonts.basic, fontSize: 20, textAlign: 'center', margin: 0, padding: 0}}>
-                            {points ? ("Spread: " + points + " ") : "No Spread Betting"}
-                        </p>
-                        <p style={{color: 'darkgrey', fontWeight: 500, fontSize: 20,  margin: 0, padding: 0, textAlign: 'center'}}>
-                            {points ? spreadLine : null}
-                        </p>
-                    </div>
-                </OstCard>
-            )
-        }
-
         // Main Render for Betting Area
         function renderBetCardArea(){
             if (outOfCredits){
@@ -433,6 +320,157 @@ export default function BetBotProjectPage() {
                 )
             }
         }
+
+        //
+        // Bet Cards 
+
+
+            // Renders all FanDuel Bets as individual Cards
+            function renderBetCards(){
+                if (!bets || bets.length < 1){
+                    return
+                }
+                let fullRenderList = []
+                let row = []
+                let i = 0
+                bets.forEach((betCard, index) => {
+                    const awayLine = betCard?.bet?.moneyline?.[betCard.away] ? betCard?.bet?.moneyline?.[betCard.away] : false
+                    const homeLine = betCard?.bet?.moneyline?.[betCard.home] ? betCard?.bet?.moneyline?.[betCard.home] : false
+
+                    const awayDiff = betCard?.bet?.spread?.[betCard.away]?.diff ? betCard?.bet?.spread?.[betCard.away]?.diff : false
+                    const homeDiff = betCard?.bet?.spread?.[betCard.home]?.diff ? betCard?.bet?.spread?.[betCard.home]?.diff : false
+
+                    const awaySpreadOdds = betCard?.bet?.spread?.[betCard.away]?.odds ? betCard?.bet?.spread?.[betCard.away]?.odds : "No Spread Betting"
+                    const homeSpreadOdds = betCard?.bet?.spread?.[betCard.home]?.odds ? betCard?.bet?.spread?.[betCard.home]?.odds : "No Spread Betting"
+
+                    const tipoff = betCard.tipoff
+
+                    // if (!awayLine && !awayDiff){
+                    //     return false
+                    // }
+
+                    row = [
+                        ...row, 
+                        (
+                        <OstCard 
+                        style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', flex: 4}}
+                        >
+                            <div style={{borderRadius: 15, paddingBottom: 0, marginBottom: 15, paddingRight: '3.3%', paddingLeft: '3.3%', backgroundColor: "#11013b"}}>
+                                <p style={{...Styles.Fonts.basic, color: '#efefef', lineHeight: 1, margin: 2, padding: 2, fontSize: 24, overflow: "hidden"}}>
+                                    {betCard.away} @ {betCard.home}
+                                </p>
+                                <p style={{...Styles.Fonts.basic, color: '#efefef', lineHeight: 1, margin: 2, padding: 2, paddingTop: 5}}>
+                                    {renderDate(betCard.tipoff)}
+                                </p>
+                            </div>
+                            <div style={{display: 'flex', flexDirection: 'row', gap: 10}}>
+                                {renderTeamBetCard(betCard.away, awayLine, awayDiff, awaySpreadOdds, tipoff)}
+                                {renderTeamBetCard(betCard.home, homeLine, homeDiff, homeSpreadOdds)}
+                            </div>
+                        </OstCard>
+                        )
+                    ]
+                    i = i + 1
+                    if (i == 3 || index >= bets.length - 1){
+                        fullRenderList = [
+                            ...fullRenderList,
+                            (
+                                <div style={{
+                                    display: 'flex', flexDirection: 'row', gap: 33,
+                                    width: '95%', marginLeft: '2.5%', 
+                                    borderTop: "4px solid #11013b", marginBottom: 20, paddingTop: 20
+                                }}>
+                                    {row}
+                                </div>
+                            )
+                        ]
+                        row = []
+                        i = 0
+                    }
+                })
+                return fullRenderList  
+            }
+
+            // Takes UTC Date String and Returns it Legibly
+            function renderDate(dateTime){
+                let dateArray = dateTime.split("-")
+                let time = dateArray[2].split("T")[1]
+                dateArray[2] = dateArray[2].split("T")[0]
+                dateArray = [...dateArray, time]
+                return `${dateArray[1]}-${dateArray[2]} at ${renderTime(dateArray[3])}`
+            }
+
+            // Takes Military Time and Converts it 
+            function renderTime(time){
+                let ampm = "am"
+                let hour = parseInt(time.split(":")[0], 10)
+                if (hour > 12){
+                    hour = hour - 12
+                    ampm = "pm"
+                }
+                if (hour == 0){
+                    hour = 12
+                }
+                return `${hour}:${time.split(":")[1]}${ampm}`
+            }
+
+            // Renders a single Moneyline / Spread Sheet
+            function renderTeamBetCard(team, line, points, spreadLine){
+                return(
+                    <OstCard 
+                    style={{flex: 5, height: 230}}
+                    onClick={(team, line, points, spread) => {
+                        setStagedBetSheet({team:team, line:line, points:points, spread:spread})
+                    }}
+                    >
+                        <p style={{...Styles.Fonts.h2, fontSize: 28, paddingBottom: 10, height: 40}}>
+                            {team}
+                        </p>
+                        <p style={{...Styles.Fonts.basic, fontSize: 20, textAlign: 'center'}}>
+                            Moneyline: <span style={{color: 'darkgrey', fontWeight: 500}}>{line ? line : "No Moneyline"}</span>
+                        </p>
+                        <div>
+                            <p style={{...Styles.Fonts.basic, fontSize: 20, textAlign: 'center', margin: 0, padding: 0}}>
+                                {points ? ("Spread: " + points + " ") : "No Spread Betting"}
+                            </p>
+                            <p style={{color: 'darkgrey', fontWeight: 500, fontSize: 20,  margin: 0, padding: 0, textAlign: 'center'}}>
+                                {points ? spreadLine : null}
+                            </p>
+                        </div>
+                    </OstCard>
+                )
+            }
+
+        //
+        // Selected Bets
+
+            // Renders Add Bet Modal
+            function renderAddbetModal(){
+                return (
+                    <ReactModal
+                    isOpen={addBetModal}
+                    >
+                        <div>
+                            <h2>Selected Team: {stagedBetSheet.team}</h2>
+                            <div style={{display: 'flex', flexDirection: 'row'}}>
+                                <OstCard>
+                                    <p style={{...Styles.Fonts.basic, fontSize: 20, textAlign: 'center'}}>
+                                       Take Moneyline: <span style={{color: 'darkgrey', fontWeight: 500}}>{line ? line : "No Moneyline"}</span>
+                                    </p>
+                                </OstCard>
+                                <OstCard>
+                                    <p style={{...Styles.Fonts.basic, fontSize: 20, textAlign: 'center', margin: 0, padding: 0}}>
+                                        {points ? ("Take Spread: " + points + " ") : "No Spread Betting"}
+                                    </p>
+                                    <p style={{color: 'darkgrey', fontWeight: 500, fontSize: 20,  margin: 0, padding: 0, textAlign: 'center'}}>
+                                        {points ? spreadLine : null}
+                                    </p>
+                                </OstCard>
+                            </div>
+                        </div>
+                    </ReactModal>
+                )
+            }
 
 
     /////////////////
