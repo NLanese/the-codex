11c5078d04e5eb010381aa120ac4cedfa22b8bf1
Nick A Lanese <58953235+NLanese@users.noreply.github.com>
2025-07-30 20:06:22 -0400

    Update menu.js

--- pages/showcases/ostrich-components/menu.js
+++ pages/showcases/ostrich-components/menu.js
@@ -6,16 +6,16 @@ import { useRecoilState } from "recoil";
 import { tokenState, tabBarState, directoryDataState } from "../../../recoil/atoms";
 
 // Styles 
-import Styles from "../../../styles/styles";
+// import Styles from "../../../styles/styles";
 
 // Ostrich
-import { OstCard } from "../../../OstrichComponents/Format/OstCard";
-import { OstrichDropDown } from "../../../OstrichComponents/Dropdown/OstrichDropDown";
-import { OstrichTabBar } from "../../../OstrichComponents/Tabs/OstrichTabBar";
-import { HeaderBar } from "../../../components/Global/headerBar";
+// import { OstCard } from "../../../OstrichComponents/Format/OstCard";
+// import { OstrichDropDown } from "../../../OstrichComponents/Dropdown/OstrichDropDown";
+// import { OstrichTabBar } from "../../../OstrichComponents/Tabs/OstrichTabBar";
+// import { HeaderBar } from "../../../components/Global/headerBar";
 
 // Assets
-const OstCompImg = "../../../assets/icons/big/OstrichComponentsLogo.png";
+// const OstCompImg = "../../../assets/icons/big/OstrichComponentsLogo.png";
 
 export default function OstrichShowcaseMenu() {
 
