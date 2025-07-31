185d5b49df5d9edae80d395129da32d4809c7e26
Nick A Lanese <58953235+NLanese@users.noreply.github.com>
2025-07-31 00:43:59 -0400

    Update tabBars.js

--- components/ostrich-components/tabBars.js
+++ components/ostrich-components/tabBars.js
@@ -1,7 +1,7 @@
 import React, {useEffect, useState} from "react";
 import Styles from "../../styles/styles";
 
-import { HeaderBar } from "./components/Global/headerBar";
+import { HeaderBar } from "../Global/headerBar";
  
 
 // Ostrich
