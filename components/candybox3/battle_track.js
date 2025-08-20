a4bac5cbd963e83cdac8f2698bf5149859f3a1aa
Nick A Lanese <58953235+NLanese@users.noreply.github.com>
2025-08-20 00:27:35 -0400

    items

---
+++ components/candybox3/battle_track.js
@@ -0,0 +1,34 @@
+export default function BATTLE_TRACK({
+    selectedMap, 
+    setSelectedMap,
+    inventory,
+    setInventory,
+    thingsDone, 
+    setThingsDone,
+    setCandies,
+    setLollipops,
+    selectedWeapon,
+    selectedArmor,
+    selectedBoots,
+    selectedTrinkets,
+    knownSpells,
+    knownPotions,
+    knownSkills
+}) {
+
+    ///////////
+    // State //
+    ///////////
+
+    // Player Token
+    const [token, seToken] = useState(
+        <div>
+            <p>\ O / </p>
+            <p> `| </p>
+            <p> / \  </p>
+        </div>
+    )
+    function determineToken(){
+
+    }
+}
--- components/candybox3/const/items/NewOldsVilleForge.js
+++ components/candybox3/const/items/NewOldsVilleForge.js
@@ -1,52 +1,67 @@
 const NewOldsForgeItems = [
     {
         name: "Mace",
+        desc: "A big ol' mace. Smacks people really hard, but its pretty slow.",
         cost: 5000000,
         id: "mace",
         status: "base",
-        type: "weapon"
+        type: "weapon",
+        damage: 25,
+        speed: 2500
     },
     {
         name: "Silver Sword",
+        desc: "A very shiney sword, perfectly balanced. It swings very quickly and does good damage.",
         cost: 10000,
         id: "sword2",
         status: "base",
-        type: "weapon"
+        type: "weapon",
+        damage: 7,
+        speed: 1000
     },
     {
         name: "Iron Axe",
+        desc: "A hardy iron axe. Very good for chopping woord, even better for chopping bad guys.",
         cost: 1500,
         id: "axe",
         status: "base",
-        type: "weapon"
+        type: "weapon",
+        damage: 5,
+        speed: 2000
     },
     {
         name: "Wooden Sword",
+        desc: "A wooden sword. It kinda sucks, but it will do.",
         cost: 350,
         id: "sword1",
         status: "base",
-        type: "weapon"
+        type: "weapon",
+        damage: 2,
+        speed: 1500
     },
     {
         name: "Leather Armor",
         cost: 10000,
         id: "leather",
         status: "base",
-        type: "armor"
+        type: "armor",
+        reduction: 0.3
     },
     {
         name: "Iron Armor",
         cost: 115000,
         id: "iron",
         status: "base",
-        type: "armor"
+        type: "armor",
+        reduction: 0.55
     },
     {
         name: "Space Age Steel",
         cost: 10000000,
         id: "sas",
         status: "base",
-        type: "armor"
+        type: "armor",
+        reduction: 0.75
     },
     
 ]
