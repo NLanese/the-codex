ca28e29c9f6b3e92ba3fca3b1d25f6228ea5279e
Nick A Lanese <58953235+NLanese@users.noreply.github.com>
2025-10-20 17:30:27 -0400

    Update portfolioLists.js

--- constants/portfolioLists.js
+++ constants/portfolioLists.js
@@ -1,10 +1,8 @@
 const onGoing = [
-    [
-        "Boardgame Master AI", 
-        "BetBot",
-        {title: "Ostrich Component Library", onClick: () => router.replace("/showcases/ostrich-components/menu"), manualOnClick: true},
-        "CandyBox 3"
-    ]
+    "Boardgame Master AI", 
+    "BetBot",
+    {title: "Ostrich Component Library", onClick: () => router.replace("/showcases/ostrich-components/menu"), manualOnClick: true},
+    "CandyBox 3"
 ]
 
 const proWorks = [
