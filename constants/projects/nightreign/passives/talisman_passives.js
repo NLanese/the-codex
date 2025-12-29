f9d042c18e1bec22d155906c138287128d2df84c
Nick A Lanese <58953235+NLanese@users.noreply.github.com>
2025-12-29 13:45:59 -0500

    Update talisman_passives.js

--- constants/projects/nightreign/passives/talisman_passives.js
+++ constants/projects/nightreign/passives/talisman_passives.js
@@ -72,12 +72,90 @@ const talismans = [
     },
     {
         title: "Winged Sword Insignia",
-        desc: "Successive Attacks Boost Power. This effect stacks on procs, going from 3 to 5 to 10% damage increase",
+        desc: "Successive Attacks Boost Power. This effect stacks on procs, going from 3 to 5 to 10% damage increase. Bows do not trigger, Carian Slicer is the only spell that does.",
         stacks: {self: true, selfType: true},
         selfType: "successiveHits",
         effect: {
             always: false,
             condition: "After 3 Stacks of Successive Hits",
+            allDamage: 1.10,
+        }
+    },
+    {
+        title: "Millicent's Prothesis",
+        desc: "Improves Dex by 5 and Successive Attacks Boost Power. This effect stacks on procs, going from 4 to 6 to 11% damage increase. Bows do not trigger, Carian Slicer is the only spell that does.",
+        stacks: {self: true, selfType: true},
+        selfType: "statChange",
+        effect: {
+            always: false,
+            condition: "After 3 Stacks of Successive Hits",
+            allDamage: 1.11,
+            dex: 5
+        }
+    },
+    {
+        title: "Magic Scorpion Charm",
+        desc: "Improves Magic Damage 15% but you take 3% more physical damage.",
+        stacks: {self: true, selfType: true},
+        selfType: "scorpion",
+        effect: {
+            always: true,
+            magicDamage: 1.15,
+            physNegation: 0.97
+        }
+    },
+    {
+        title: "Fire Scorpion Charm",
+        desc: "Improves Fire Damage 15% but you take 3% more physical damage.",
+        stacks: {self: true, selfType: true},
+        selfType: "scorpion",
+        effect: {
+            always: true,
+            fireDamage: 1.15,
+            physNegation: 0.97
+        }
+    },
+    {
+        title: "Lightning Scorpion Charm",
+        desc: "Improves Lightning Damage 15% but you take 3% more physical damage.",
+        stacks: {self: true, selfType: true},
+        selfType: "scorpion",
+        effect: {
+            always: true,
+            lightningDamage: 1.15,
+            physNegation: 0.97
+        }
+    },
+    {
+        title: "Sacred Scorpion Charm",
+        desc: "Improves Holy Damage 15% but you take 3% more physical damage.",
+        stacks: {self: true, selfType: true},
+        selfType: "scorpion",
+        effect: {
+            always: true,
+            holyDamage: 1.15,
+            physNegation: 0.97
+        }
+    },
+    {
+        title: "Red-Feathered Branchsword",
+        desc: "Improves Attack Power by 20% at when below 20% HP.",
+        stacks: {self: true, selfType: true},
+        selfType: "scorpion",
+        effect: {
+            always: false,
+            condition: "When below 20% HP",
+            allDamage: 1.20,
+        }
+    },
+    {
+        title: "Ritual Sword Talisman",
+        desc: "Improves Attack Power by 10% at Full HP.",
+        stacks: {self: true, selfType: true},
+        selfType: "scorpion",
+        effect: {
+            always: false,
+            condition: "When at Full HP",
             allDamage: 1.10,
         }
     },
